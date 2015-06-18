de98f56a6648e8de1dc7dc7f59795a911ce668b6
Sameer Rahmani <lxsameer@gnu.org>
2015-06-18 14:10:45 +0430

    sitecategory seed added

---
+++ db/migrate/20150618093838_create_site_categories.rb
@@ -0,0 +1,10 @@
+class CreateSiteCategories < ActiveRecord::Migration
+  def change
+    create_table :site_categories do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
