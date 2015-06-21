802654cc9ee4ee247875fb0dcf8acdd2251e400a
Behnam Ahmad Khan Beigi <yottanami@gnu.org>
2015-06-21 13:29:23 +0430

    sites controller modified

--- app/controllers/dashboard/sites_controller.rb
+++ app/controllers/dashboard/sites_controller.rb
@@ -4,13 +4,26 @@ module Dashboard
     # GET /sites/new
     def new
       @gears = Gear.all
-      @siteCategory = SiteCategory.all
+      @siteCategories = SiteCategory.all
+      @namespaces = Namespace.where(owner_id: current_user.ir)
+      @domains = SiteFramework.Domain.all
     end
 
     # POST /sites
     # POST /sites.json
     def create
+      @site = SiteFramework::Site.create!(title: params[:title],
+                                      site_category_id: params[:site_category_id],
+                                      owner_id: current_user.ir)
       authorize @site
+      @domain = SiteFramework::Domain.create!(name: params[:name],
+                                              parent_id: params[:parent_id],
+                                              namespace_id: namespace_id,
+                                              site: @site)
+      @gear_box = GearBox.create!(create!(site: @site,
+                                          gear_id: params[:gear_id]),
+                                  user_id: current_user.id
+                                  )
       respond_to do |format|
         if @site.save
           format.html { redirect_to @site, notice: 'Site was successfully created.' }
@@ -23,15 +36,11 @@ module Dashboard
     end
 
     def build_resource
-      @site = SiteFramework::Site.new(params)
-      @site.owner_id = current_user
-      @domain = SiteFramework::Domain.new
-      @gear_box = GearBox.new
 
     end
 
     def resource_params
-      params.require(:site).permis(:id, :name, :site_category_id, :gear_box)
+      params.require(:site).permis(:id, :title, :url, :site_category_id, :gear_box)
     end
   end
 end
